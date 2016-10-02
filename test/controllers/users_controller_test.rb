65426364624dcac24d6100f0211de5a4723c6d83
Dai <kdmgs110@gmail.com>
2016-10-02 08:58:44 +0000

    finished chapter 7

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,58 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+  
+  def setup
+    @user = User.new(name:"Example User", email:"user@example.com",password:"foobar",password_confirmation:"foobar")
+  end
+  
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+  
+  test "name should be present" do 
+    @user.name =" "
+    assert_not @user.valid?
+  end
+  
+  test "email should be present" do 
+    @user.email =" "
+    assert_not @user.valid?
+  end
+  
+  test "name shoud not too be long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+  
+  test "email shoud not too be long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+  
+  test "email validation should accept valid addresses" do 
+    valid_addresses = %w[user@example.com USERS@foo.COM A_US_ER@foo.bar.org first.last@foo.jp alice+boo@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?
+    end
+  end
+    
+  test "email addresses should be unique" do 
+      duplicate_user  = @user.dup
+      duplicate_user.email = @user.email.upcase
+      @user.save 
+      assert_not duplicate_user.valid?
+    end
+    
+  test "password should be present" do 
+    @user.password = @user.password_confirmation = "" * 6
+    assert_not @user.valid?
+  end
+  
+  test "password have minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+end
